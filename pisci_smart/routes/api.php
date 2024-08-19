ca4ac2afdb08b67de69b2e11bde8f71a752d5fb8
Arhamatou24 <127671445+Arhamatou24@users.noreply.github.com>
2024-08-19 18:53:50 +0200

    crud
    crud

--- pisci_smart/routes/api.php
+++ pisci_smart/routes/api.php
@@ -1,67 +1,47 @@
 <?php
 
-use App\Http\Controllers\EmployeController;
-use App\Http\Controllers\NourritureController;
-use App\Http\Controllers\PisciculteurController;
-use App\Http\Controllers\VisiteurController;
-use App\Http\Controllers\AdministrateurController;
-use App\Http\Controllers\DispositifController;
-
-
-use App\Models\Pisciculteur;
-use App\Models\Nourriture;
-use App\Models\Empploye;
-use App\Models\Visiteur;
-use App\Models\Administrateur;
-use App\Models\Dispositif;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PisciculteurController;
+use App\Http\Controllers\EmployeController;
+use App\Http\Controllers\NourritureController;
+use App\Http\Controllers\VisiteurController;
 
-Route::get('/user', function (Request $request) {
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
 
+
+
+//picsciculteur
+Route::get('/pisciculteur', [PisciculteurController::class, 'get_all_pisciculteur']);
+Route::get('/pisciculteur/{id}', [PisciculteurController::class, 'getPisciculteurById']);
+Route::post('/pisciculteur', [PisciculteurController::class, 'create_pisciculteur']);
+Route::put('/pisciculteur/{id}', [PisciculteurController::class, 'update_pisciculteur']);
+Route::delete('/pisciculteur/{id}', [PisciculteurController::class, 'delete_pisciculteur']);
+
+//employé
+Route::get('/employe', [EmployeController::class, 'get_all_employe']);
+Route::get('/employe/{id}', [EmployeController::class, 'getEmployeById']);
+Route::post('/employe', [EmployeController::class, 'create_employe']);
+Route::put('/employe/{id}', [EmployeController::class, 'update_employe']);
+Route::delete('/employe/{id}', [EmployeController::class, 'delete_employe']);
 
 //nourriture
-Route::get("/nourriture",[NourritureController::class,'get_all_nourriture']);
+Route::get('/nourriture', [NourritureController::class, 'get_all_nourriture']);
 Route::get('/nourriture/{id}', [NourritureController::class, 'getNourritureById']);
-Route::post("/nourriture",[NourritureController::class,'create_nourriture']);
-Route::delete("/nourriture/{id}",[NourritureController::class,'delete_nourriture']);
+Route::post('/nourriture', [NourritureController::class, 'create_nourriture']);
 Route::put('/nourriture/{id}', [NourritureController::class, 'update_nourriture']);
+Route::delete('/nourriture/{id}', [NourritureController::class, 'delete_nourriture']);
 
-
-//pisciculteur
-Route::get("/pisciculteur",[PisciculteurController::class,'get_all_pisciculteur']);
-Route::get('/pisciculteur/{id}', [PisciculteurController::class, 'getPisciculteurById']);
-Route::post("/pisciculteur",[PisciculteurController::class,'create_pisciculteur']);
-Route::delete("/pisciculteur/{id}",[PisciculteurController::class,'delete_pisciculteur']);
-Route::put('/pisciculteur/{id}', [PisciculteurController::class, 'update_pisciculteur']);
-
-//employe
-Route::get("/employe",[EmployeController::class,'get_all_employe']);
-Route::get('/employe/{id}', [EmployeController::class, 'getEmployeById']);
-Route::post("/employe",[EmployeController::class,'create_employe']);
-Route::delete("/employe/{idEmploye}",[EmployeController::class,'delete_employe']);
-Route::put('/employe/{idEmploye}', [EmployeController::class, 'update_employe']);
 
 //visiteur
-Route::get("/visiteur",[VisiteurController::class,'get_all_visiteur']);
+Route::get('/visiteur', [VisiteurController::class, 'get_all_visiteur']);
 Route::get('/visiteur/{id}', [VisiteurController::class, 'getVisiteurById']);
-Route::post("/visiteur",[VisiteurController::class,'create_visiteur']);
-Route::delete("/visiteur/{idVisiteur}",[VisiteurController::class,'delete_visiteur']);
-Route::put('/visiteur/{idVisiteur}', [VisiteurController::class, 'update_visiteur']);
-
-//bassin
-
-
-//administrateur
-Route::get('/administrateur', [AdministrateurController::class, 'get_all_admin']);
-Route::get('/administrateur/{id}', [AdministrateurController::class, 'getAdministrateurById']);
-Route::post('/administrateur', [AdministrateurController::class, 'store']);
-Route::delete('/administrateur/{idAdmi}', [AdministrateurController::class, 'destroy']);
-Route::put('/administrateur/{idAdmi}', [AdministrateurController::class, 'update']);
-
-//dispositif
+Route::post('/visiteur', [VisiteurController::class, 'create_visiteur']);
+Route::put('/visiteur/{id}', [VisiteurController::class, 'update_visiteur']);
+Route::delete('/visiteur/{id}', [VisiteurController::class, 'delete_visiteur']);
 
 
