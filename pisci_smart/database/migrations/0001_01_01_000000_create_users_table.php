e7b99e82d514d1ec7bd26dfdd76b99853b3ae354
Ephraim D Oladokun <oladokunefi123@gmail.com>
2024-09-09 10:53:37 +0000

    kk

--- pisci_smart/database/migrations/0001_01_01_000000_create_users_table.php
+++ pisci_smart/database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('nom');
             $table->string('prenom');
             $table->string('telephone')->unique(); // Numéro de téléphone unique
-            //$table->string('adresse')->nullable();
+            $table->string('adresse')->nullable();
             // $table->string('role'); // Administrateur, Pisciculteur, Visiteur, Employé
             $table->string('password');
             $table->rememberToken();
